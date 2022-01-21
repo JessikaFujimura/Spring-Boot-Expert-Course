e81ead76ddecf859161d1376041f06ceb018924c
Jéssika <jessikafujimura@gmail.com>
2022-01-20 23:11:45 -0300

    interfaces do jpa

--- src/main/java/io/github/jessikafujimura/SpringBootExpertCourseApplication.java
+++ src/main/java/io/github/jessikafujimura/SpringBootExpertCourseApplication.java
@@ -1,49 +1,40 @@
 package io.github.jessikafujimura;
 
 import io.github.jessikafujimura.domain.entity.Cliente;
+import io.github.jessikafujimura.domain.entity.Pedido;
 import io.github.jessikafujimura.domain.repository.Clientes;
+import io.github.jessikafujimura.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class SpringBootExpertCourseApplication {
 
     @Bean
-    public CommandLineRunner init (@Autowired Clientes clientes){
+    public CommandLineRunner init (@Autowired Clientes clientes,
+                                   @Autowired Pedidos pedidos){
         return args -> {
             Cliente cliente = new Cliente("Jéssika");
-            clientes.salvar(cliente);
+            clientes.save(cliente);
 
-            Cliente cliente2 = new Cliente("Yami");
-            clientes.salvar(cliente2);
 
-            List<Cliente> todos = clientes.obterTodosCliente();
-            todos.forEach(System.out::println);
+            Pedido pedido = new Pedido();
+            pedido.setCliente(cliente);
+            pedido.setDataPedido(LocalDate.now());
+            pedido.setTotal(BigDecimal.valueOf(100));
+            pedidos.save(pedido);
 
-            todos.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado");
-                clientes.atualizar(c);
-            }
-            );
+//            Cliente cli = clientes.findClienteFetchPedido(cliente.getId());
+//            System.out.println(cli);
+//            System.out.println(cli.getPedidos());
 
-            todos = clientes.obterTodosCliente();
-            clientes.buscarPorNome("ka").forEach(System.out::println);
-
-            todos.forEach(i -> {
-                clientes.deletar(i);
-            });
-
-            todos = clientes.obterTodosCliente();
-            if(todos.isEmpty()){
-                System.out.println("Nenhum cliente encontrado");
-            } else {
-                todos.forEach(System.out::println);
-            }
+            pedidos.findByCliente(cliente).forEach(System.out::println);
 
         };
     }
--- src/main/java/io/github/jessikafujimura/domain/repository/Clientes.java
+++ src/main/java/io/github/jessikafujimura/domain/repository/Clientes.java
@@ -1,57 +1,25 @@
 package io.github.jessikafujimura.domain.repository;
 
 import io.github.jessikafujimura.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
-@Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Autowired
-    private EntityManager entityManager;
+    @Query(value = "select c from Cliente c where c.nome like :nome ")
+    List<Cliente> encontrarPorNome(@Param("nome") String ka);
 
-    @Transactional
-    public Cliente salvar(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    };
+    @Query("delete from Cliente c where c.nome =:nome")
+    @Modifying
+    void deleteByNome(String nome);
 
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodosCliente(){
-        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
+    boolean existsByNome(String nome);
 
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome){
-        String jpql = "select c from Cliente c where c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome +"%");
-        return query.getResultList();
-    }
-
-    @Transactional
-    public Cliente atualizar(Cliente cliente){
-       entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletarPorId(Integer id) {
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        entityManager.remove(cliente);
-    }
+    @Query(" select c from Cliente c left join fetch c.pedidos p where c.id = :id")
+    Cliente findClienteFetchPedido(@Param("id") Integer id);
 
 }
